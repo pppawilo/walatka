70e05a810bd7fd4e9ee4683983cf4656ea55642c
Paweł Pawiłojć <pawilojc@gmail.com>
2020-04-09 16:54:45 +0200

    refactor fnc

--- walatka.js
+++ walatka.js
@@ -4,29 +4,35 @@ function checkPermission() {
     var accelerationZ = 0;
 
     var startTime = 0;
-    document.getElementById("demo").innerHTML = d.getTime();
+
     if (typeof DeviceMotionEvent.requestPermission === 'function') {
         DeviceMotionEvent.requestPermission()
             .then(response => {
                 if (response == 'granted') {
-                    startTime = Date.now();
-                    motionEvent();
+                    motionEventListener();
                 }
             })
             .catch(console.error)
 
     } else {
-        motionEvent();
+        motionEventListener();
 
 
     }
 
 
-    function motionEvent() {
+
+    function motionEventListener() {
+        startTime = Date.now();
+        document.getElementById("accelerationXfinal").innerHTML = "x ";
+        document.getElementById("accelerationYfinal").innerHTML = "y ";
+        document.getElementById("accelerationZfinal").innerHTML = "z ";
         window.addEventListener('devicemotion', function (event) {
-            if (Date.now() < startTime + 3000) {
+
+            if (Date.now() < (startTime + 3000)) {
                 motionHandler(event);
-            } else {
+            }
+            else {
                 document.getElementById("accelerationXfinal").innerHTML = "x " + accelerationX;
                 document.getElementById("accelerationYfinal").innerHTML = "y " + accelerationY;
                 document.getElementById("accelerationZfinal").innerHTML = "z " + accelerationZ;
@@ -36,14 +42,14 @@ function checkPermission() {
 
     function motionHandler(event) {
 
-        if (event.acceleration.x > accelerationX) {
+        if (Math.abs(event.acceleration.x) > accelerationX) {
             accelerationX = event.acceleration.x;
         }
-        if (event.acceleration.y > accelerationY) {
+        if (Math.abs(event.acceleration.y) > accelerationY) {
             accelerationY = event.acceleration.y;
         }
-        if (event.acceleration.z > accelerationZ) {
-            accelerationZ = event.acceleration.Z;
+        if (Math.abs(event.acceleration.z) > accelerationZ) {
+            accelerationZ = event.acceleration.z;
         }
         document.getElementById("accelerationX").innerHTML = "x " + event.acceleration.x;
         document.getElementById("accelerationY").innerHTML = "y " + event.acceleration.y;
